135190f958403b9cddd5e4f150c7d415894e0bf2
Nadin <lizal15061983@gmail.com>
2026-08-30 04:14:54 +0300

    JS домашка

---
+++ hw2.js
@@ -0,0 +1,70 @@
+//Задание 1
+
+let a = 10;
+alert(a);
+
+a = 20;
+alert(a);
+
+//Задание 2
+//Год выпуска первого айфона
+
+const  iphoneReLeaseYear = 2007;
+alert(iphoneReLeaseYear);
+
+//Задание 3
+
+const jsCreator = "Brendan Eich";
+alert(jsCreator);
+
+//Задание 4
+
+let b = 10;
+let c = 2;
+
+ alert(b+c);
+ alert(b-c);
+ alert(b*c);
+ alert(b/c);
+
+ //Задание 5
+ // Возвожу в степень
+
+ let result = 2 ** 5;
+ alert(result);
+
+ //Задание 6
+ //Находим остаток от деления
+
+ a = 9;
+ b = 2;
+ alert(a % b);
+
+ //Задание 7
+
+ let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+//Задание 8
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+//Задание 9
+
+const user = {
+    name: 'Анна',
+    age: 18,
+    isAdmin: true
+};
+ //Задание 10 
+
+ //Запрос имени пользователя
+ let userName = prompt('Как ваше имя?');
+ alert(`Привет, ${userName}!`);
